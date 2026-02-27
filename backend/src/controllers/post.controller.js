d49edf70883cab9596ad86018c31168f01d2feaa
kairo_name <kaironaser100@gmail.com>
2026-02-27 11:55:22 +0300

    Implement CRUD functionality for posts with routes for creating, retrieving, updating, and deleting posts

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -33,3 +33,55 @@ res.status(400).json({
 }
 }
 
+export async function getPosts(req, res){
+    try{
+     const post = await Post.find()
+     res.status(200).json(post)
+    }
+    catch(error)
+{
+        console.log("Error creating the post: ",error )
+res.status(400).json({
+    message:"Internal Server Error"
+})
+}}
+
+export async function updatePost(req, res){
+    try{
+       if(Object.keys(req.body).length === 0){
+        return res.status(400).json({
+            message:"All fields are required"
+        })
+         }
+
+        const post = Post.findByIdAndUpdate(req.params.id, req.body,{new:true})
+         if(!post){
+            return res.status(404).json({message:"post not found"})
+         }
+         res.status(200).json({message:"Post update successfully "})
+   
+    }
+    catch(error)
+{
+    console.log("Error of updating post: ", error)
+    res.status(500).json({
+        message:"Failed to update post"
+    })
+}}
+
+export async function deletePost(req, res){
+    try{
+      const post = Post.findByIdAndDelete(req.params.id)
+      if(!post) return res.status(404).json({message:"The post not found"})
+      res.status(200).json({
+      message:"The Post Deleted",
+      Post
+})
+    }
+    catch(error){
+        console.log("Error deleting post: ", error)
+        res.status(500).json({
+            message:"Internal Server Error"
+        })
+    }
+}
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, deletePost, getPosts, updatePost } from "../controllers/post.controller.js";
 
 const postRouter = Router();
 postRouter.route('/create').post(createPost)
+postRouter.route('/getPosts').get(getPosts)
+postRouter.route('/update/:id').patch(updatePost)
+postRouter.route('/delete/:id').delete(deletePost)
 export default postRouter;
